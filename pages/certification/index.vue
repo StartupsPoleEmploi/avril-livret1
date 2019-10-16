9ecb954f42fbe16c586864d23104d422aef7f1c8
dmassiani <davidmassianirennes@gmail.com>
2019-10-16 14:47:54 +0200

    update

--- pages/certification/index.vue
+++ pages/certification/index.vue
@@ -22,6 +22,7 @@
       <div class="form-help-content">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
+      <a href="#" class="button is-text">J'ai besoin de plus d'aide</a>
     </div>
   </div>
 </template>
--- pages/experiences/periode.vue
+++ pages/experiences/periode.vue
@@ -34,6 +34,9 @@
         <div class="form-help-content">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
+        <p style="margin-top:1rem">
+          <a href="#" class="is-text">J'ai besoin de plus d'aide</a>
+        </p>
       </div>
     </div>
 </template>
