a6cc23efc8991e3956eb860aa4f4442cbf2d86c9
Marie-briche <56580765+Marie-briche@users.noreply.github.com>
2019-10-17 16:18:14 +0200

    Update index.vue

--- pages/formations/index.vue
+++ pages/formations/index.vue
@@ -33,8 +33,8 @@
       <div class="form-help">
         <h3 class="title is-4">Besoin d'aide ?</h3>
         <div class="form-help-content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+Sélectionnez la bonne case. Dites nous simplement quelle a été la dernière classe que vous avez fréquentée. Ne vous inquiétez pas, il s'agit d'une simple information, cela ne sert pas à évaluer votre candidature à la VAE.
+Rappelez-vous, la seule condition pour demander votre diplôme en VAE est de justifier d'un an d'expérience.        </div>
         <p style="margin-top:1rem">
           <a href="#" class="is-text">J'ai besoin de plus d'aide</a>
         </p>
