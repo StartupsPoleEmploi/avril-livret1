8d1aa675f9337ea6be537375d0f3c947877100f2
dmassiani <davidmassianirennes@gmail.com>
2019-10-17 21:20:37 +0200

    fix autre template

--- pages/formations/autre.vue
+++ pages/formations/autre.vue
@@ -33,7 +33,9 @@
 Dites nous si vous avez déjà chercher à obtenir un diplôme mais que vous n'en avez validé qu'une partie.
 Si l'organisme, l'école ou le centre de formation qui vous suivait pour ce diplôme , vous a donné une attestation ou un justificatif pour les parties de diplôme obtenue, cochez "oui".
 Ne vous inquiétez pas, il s'agit d'une simple information, cela ne sert pas à évaluer votre candidature à la VAE.
-Rappelez-vous, la seule condition pour demander votre diplôme en VAE est de justifier d'un an d'expérience.      <p style="margin-top:1rem">
+Rappelez-vous, la seule condition pour demander votre diplôme en VAE est de justifier d'un an d'expérience.
+      </div>
+      <p style="margin-top:1rem">
         <a href="#" class="is-text">J'ai besoin de plus d'aide</a>
       </p>
     </div>
