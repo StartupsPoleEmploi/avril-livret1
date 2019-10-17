fe125e3ccf3bf3b769745555d29c2ee9b2083c72
veroAvril <47114491+veroAvril@users.noreply.github.com>
2019-10-17 16:24:37 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
             </div>
             <div class="swiper-slide">
               <p class="title is-4">
-                Munissez-vous de vos bulletins de salaire
+               Si possible, munissez-vous de vos bulletins de salaire
               </p>
             </div>
             <div class="swiper-slide">
