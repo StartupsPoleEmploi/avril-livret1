16fed81543de5b9674666a0f4b2fcac09e2606e8
Marie-briche <56580765+Marie-briche@users.noreply.github.com>
2019-10-24 12:24:00 +0200

    Update famille.vue

--- pages/experiences/famille.vue
+++ pages/experiences/famille.vue
@@ -87,7 +87,7 @@
       <div class="form-help">
         <h3 class="title is-4">Besoin d'aide ?</h3>
         <div class="form-help-content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Il s'agit de cocher la case de la bonne famille de métier, celle de votre métier. Par exemple, si vous avez été secrétaire, la famille métier sera "gestion, administration des entreprises.
         </div>
         <p style="margin-top:1rem">
           <a href="#" class="is-text">J'ai besoin de plus d'aide</a>
