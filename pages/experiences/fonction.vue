3add762cf6b1a80ad9d579acc32c0abc22b849a7
veroAvril <47114491+veroAvril@users.noreply.github.com>
2019-10-24 15:36:09 +0200

    Update fonction.vue

--- pages/experiences/fonction.vue
+++ pages/experiences/fonction.vue
@@ -43,7 +43,7 @@
     <div class="form-help">
       <h3 class="title is-4">Besoin d'aide ?</h3>
       <div class="form-help-content">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+         Préciser votre métier, fonction, poste ou emploi. Celui en lien avec le diplôme que vous souhaitez obtenir. Vous pouvez recopier l'intitulé écrit sur votre fiche de salaire. Vous pourrez ajouter plusieurs fonctions selon les postes que vous avez occupés.
       </div>
       <p style="margin-top:1rem">
         <a href="#" class="is-text">J'ai besoin de plus d'aide</a>
