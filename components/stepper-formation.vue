c8c2710d9735d06da96d14d311819405f8d5895e
Marie-briche <56580765+Marie-briche@users.noreply.github.com>
2019-10-24 15:16:43 +0200

    Update stepper-formation.vue

--- components/stepper-formation.vue
+++ components/stepper-formation.vue
@@ -34,7 +34,7 @@
        <li :class="slugIndex == 11 ? 'steps-segment is-active' : 'steps-segment'">
          <nuxt-link to="/formations/rncp" :class="slugIndex == 11 ? 'steps-marker is-hollow' : 'steps-marker'"></nuxt-link>
          <div class="steps-content is-divider-content">
-           <nuxt-link class="is-size-6" to="/formations/rncp">Titres Pro</nuxt-link>
+           <nuxt-link class="is-size-6" to="/formations/rncp">Autres diplômes</nuxt-link>
          </div>
        </li>
        <li :class="slugIndex == 12 ? 'steps-segment is-active' : 'steps-segment'">
