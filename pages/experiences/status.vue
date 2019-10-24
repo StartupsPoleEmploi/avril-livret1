d8a17a2ec98f86a22f468ac57adac68bafa25a4b
veroAvril <47114491+veroAvril@users.noreply.github.com>
2019-10-24 16:06:55 +0200

    Update status.vue

--- pages/experiences/status.vue
+++ pages/experiences/status.vue
@@ -40,7 +40,7 @@
       <div class="form-help">
         <h3 class="title is-4">Besoin d'aide ?</h3>
         <div class="form-help-content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Pour cette expérience, étiez-vous employé par un patron, à votre compte, en formation une partie du temps ? Peu importe le statut, l'important est de prouvé que vous avez fait cette activité.
         </div>
         <p style="margin-top:1rem">
           <a href="#" class="is-text">J'ai besoin de plus d'aide</a>
