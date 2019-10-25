468aa6d0d47c73ff8e9d309c6ae5028cfb3c7757
veroAvril <47114491+veroAvril@users.noreply.github.com>
2019-10-25 07:54:46 +0200

    Update index.vue

--- pages/experiences/index.vue
+++ pages/experiences/index.vue
@@ -38,7 +38,7 @@
       <div class="form-help">
         <h3 class="title is-4">Besoin d'aide ?</h3>
         <div class="form-help-content">
-          Trouvez toutes les expériences en lien avec le diplôme que vous souhaitez obtenir. Si nécessaire consultez à nouveau le descriptif d'activités du diplôme (faire un lien?). Dans votre CV selectionnez les expériences utiles et reportez-le ici. Au total on vous demande d'avoir travailler au moins l'équivalent d'un an à temps plein dans des activités en lien avec le diplôme.
+          Trouvez toutes les expériences en lien avec le diplôme que vous souhaitez obtenir. Si nécessaire consultez à nouveau le descriptif d'activités du diplôme (faire un lien?). Dans votre CV selectionnez les expériences utiles et reportez-les une par une. Au total il faut avoir travailler au moins l'équivalent d'un an à temps plein dans des activités en lien avec le diplôme.
         </div>
         <p style="margin-top:1rem">
           <a href="#" class="is-text">J'ai besoin de plus d'aide</a>
