ce14cfcf393bde9301d0667714fb5f0f2e574c68
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-11-14 14:50:57 +0100

    remove comment

--- middleware/autosave.js
+++ middleware/autosave.js
@@ -2,8 +2,6 @@ const QUERY_APP_HOST = 'http://172.18.0.3:4000';
 const REDIRECT_APP_HOST = 'http://localhost:4000';
 
 export default async function ({ store, req } = context) {
-  console.log("(Global Middleware) SSR: " + process.server);
-
   if (process.client && store.state.hash) {
     const response = await fetch(`${QUERY_APP_HOST}/api/booklet?hash=${store.state.hash}`, {
       method: 'PUT',
