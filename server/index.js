254f2b4c369e477d6f223fcb988c0cc297bcb2b4
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-11-29 11:03:25 +0100

    pdf works

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const pdfGenerator = require('./pdf');
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
+
+app.get('*.pdf', pdfGenerator);
 
 async function start () {
   // Init Nuxt.js
