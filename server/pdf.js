b3f42187b4e3376294c20e3bf801d4155eb490f4
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-11-29 17:39:02 +0100

    pdf ok

--- server/pdf.js
+++ server/pdf.js
@@ -6,6 +6,15 @@ export default async (req, res) => {
   if (req.body.body) {
     pdf.create(req.body.body, {
       format: 'A4',
+      header: {
+        height: '1cm',
+        // contents: phantom.callback(function(pageNum, numPages) {
+        //   return "<h1>Header <span style='float:right'>" + pageNum + " / " + numPages + "</span></h1>";
+        // })
+      },
+      footer: {
+        height: '1cm',
+      }
     }).toFile(`./tmp/${uuid()}.pdf`, (err, pdfRes) => {
       res.sendFile(pdfRes.filename);
     });
