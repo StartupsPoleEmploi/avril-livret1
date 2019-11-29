90cb152c3381be1ef5a9de346a96f8e84c8dc6d6
dmassiani <davidmassianirennes@gmail.com>
2019-11-29 15:47:43 +0100

    add style and dignity to cerfa

---
+++ layouts/cerfa.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-10 is-offset-1">
+        <div class="avril-layout">
+          <div class="avril-">
+            <div class="avril-form-help-container">
+              <nuxt />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<style>
+</style>
