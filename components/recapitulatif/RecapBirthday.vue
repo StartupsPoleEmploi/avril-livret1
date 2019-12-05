dd50971994e64c5a632f538df056703ddeafe74b
Augustin Riedinger <augustin.riedinger@gmail.com>
2019-12-05 14:02:19 +0100

    nationality plugged

--- components/recapitulatif/RecapBirthday.vue
+++ components/recapitulatif/RecapBirthday.vue
@@ -2,6 +2,8 @@
   <span class="recap-cell cell-birthday">
     <p v-if="identite.birthday">Je suis né<span v-if="identite.sex!='m'">e</span> le {{formatDate(identite.birthday)}} à {{addressLabelify(identite.birthPlace)}}</p>
     <p v-else>Je n'ai pas encore renseigné ma date de naissance.</p>
+    <p v-if="identite.nationality.country_code">Je suis de nationalité {{identite.nationality.country_code.toUpperCase()}}.</p>
+    <p v-else>Je n'ai pas encore renseigné ma nationalité.</p>
   </span>
 </template>
 
