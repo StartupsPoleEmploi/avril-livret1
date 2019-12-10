55e123613eedac71420dc484ef1664a20525c4c1
nresni <nresnikow@gmail.com>
2019-12-10 17:39:27 +0100

    [livret1] fix certifier and certification names

--- middleware/autosave.js
+++ middleware/autosave.js
@@ -1,30 +1,36 @@
-import {storeToBackend} from '../mappers/toBackend';
+import { storeToBackend } from "../mappers/toBackend";
 
-export default async function ({ store, req, env } = context) {
+export default async function({ store, req, env } = context) {
   if (process.client && store.state.hash) {
-    const response = await fetch(`${env.apiUrl}/api/booklet?hash=${store.state.hash}`, {
-      method: 'PUT',
-      mode: 'cors',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        certificationLabel: store.state.certificationLabel,
-        certifierLabel: store.state.certifierLabel,
-        civility: storeToBackend.identity(store.state.identity),
-        experiences: storeToBackend.experiences(store.state.experiences),
-        education: storeToBackend.education(store.state.education),
-      })
-    });
+    const response = await fetch(
+      `${env.apiUrl}/api/booklet?hash=${store.state.hash}`,
+      {
+        method: "PUT",
+        mode: "cors",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          certification_name: store.state.certificationLabel,
+          certifier_name: store.state.certifierLabel,
+          civility: storeToBackend.identity(store.state.identity),
+          experiences: storeToBackend.experiences(store.state.experiences),
+          education: storeToBackend.education(store.state.education)
+        })
+      }
+    );
     if (response.ok) {
       const data = await response.json();
-      console.log(':) Saved', data);
+      console.log(":) Saved", data);
     } else {
-      console.log(':( Could not save')
+      console.log(":( Could not save");
     }
   } else {
-    console.log(process.client ? 'Not authenticated, no save' : 'Server side, no need to save')
+    console.log(
+      process.client
+        ? "Not authenticated, no save"
+        : "Server side, no need to save"
+    );
   }
 }
-
