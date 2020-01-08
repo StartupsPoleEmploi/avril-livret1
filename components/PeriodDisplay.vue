25b9a7552e167fc9548b8ec4102923a592290d31
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-01-08 16:53:35 +0100

    period round week hours

--- components/PeriodDisplay.vue
+++ components/PeriodDisplay.vue
@@ -3,7 +3,7 @@
     Du {{ formatDate(period.start) }}
     <span v-if="period.end">au {{ formatDate(period.end) }}</span>
     <span v-else>à aujourd'hui</span> :
-    <span v-if="isMoreThanOneWeek"><strong>{{period.weekHours}}h/semaine</strong> soit</span>
+    <span v-if="isMoreThanOneWeek"><strong>{{Math.round(period.weekHours)}}h/semaine</strong> soit</span>
     <strong>{{ periodTotalHours }} heures</strong> en cumulé
     <span v-if="!isMoreThanOneWeek">{{period.weekHours >= fullTimeWeekHours ? 'à temps plein' : 'à temps partiel'}}</span>
   </div>
