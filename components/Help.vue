7cad7e438de2d22abf670efb6f3094cb88f4ebb2
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-01-15 16:43:53 +0100

    help enhanced

--- components/Help.vue
+++ components/Help.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="form-help">
-    <h3 class="title is-5">Besoin d'aide ?</h3>
-    <div class="form-help-content content">
-      <div v-html="content"></div>
+    <div class="notification is-success">
+      <h3 class="title is-5">Besoin d'aide ?</h3>
+      <div class="form-help-content content">
+        <div v-html="content"></div>
+      </div>
     </div>
   </div>
 </template>
