d062abaad406d08433805e4f8c98cc1e0f026935
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-11-04 16:55:18 +0100

    expandable help image

--- components/Help.vue
+++ components/Help.vue
@@ -4,8 +4,8 @@
       <button v-if="isExpanded" @click="toggleExpanded()" class="delete is-pulled-right"></button>
       <h3 class="title is-5" @click="toggleExpanded()">Besoin d'aide ?</h3>
       <div class="form-help-content content">
-        <div v-html="content"></div>
-        <client-only>
+        <div v-html="contentNoImage"></div>
+        <client-only v-if="img">
           <expandable-image class="no-stretch bg-click" :src="img" :close-on-background-click="true"></expandable-image>
         </client-only>
       </div>
@@ -14,6 +14,19 @@
 </template>
 <script type="text/javascript">
   export default {
+    computed: {
+      img: function() {
+        if (this.content) {
+          const match = this.content.match(/<img.*?src="(.*?)".*?>/mi);
+          return match && match[1];
+        }
+      },
+      contentNoImage: function() {
+        if (this.content) {
+          return this.content.replace(/<p><img.*?<\/p>/, '');
+        }
+      },
+    },
     data: function() {
       return {
         isExpanded: false,
@@ -26,7 +39,6 @@
     },
     props: [
       'content',
-      'img',
     ],
     watch: {
       content: function(newVal, oldVal) {
@@ -36,15 +48,23 @@
   }
 </script>
 <style lang="scss">
-  body > .expandable-image.expanded.no-stretch > img {
-    width: auto;
-    height: auto;
-  }
+  .expandable-image {
+    &:not(.expanded) img {
+      cursor: pointer;
+    }
 
-  body > .expandable-image.expanded.bg-click {
-    cursor: zoom-out;
-    & > img {
-      cursor: default;
+    &.expanded {
+      &.no-stretch > img {
+        width: auto;
+        height: auto;
+      }
+      &.bg-click {
+        cursor: pointer;
+        & > img {
+          cursor: default;
+        }
+      }
     }
   }
+
 </style>
